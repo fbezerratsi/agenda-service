2b4f3f1b070f09efcf39f5fc36eeab1fabcb9b92
fbezerratsi <fbezerra.tsi@gmail.com>
2022-07-19 16:16:29 -0300

    adicionar a parte de servico

---
+++ src/main/java/micro/com/agenda/domain/repository/PacienteRepository.java
@@ -0,0 +1,10 @@
+package micro.com.agenda.domain.repository;
+
+import micro.com.agenda.domain.entity.Paciente;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PacienteRepository extends JpaRepository<Paciente, Long > {
+
+}
---
+++ src/main/java/micro/com/agenda/domain/service/PacienteService.java
@@ -0,0 +1,34 @@
+package micro.com.agenda.domain.service;
+
+import lombok.RequiredArgsConstructor;
+import micro.com.agenda.domain.entity.Paciente;
+import micro.com.agenda.domain.repository.PacienteRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class PacienteService {
+
+    private final PacienteRepository repository;
+
+    public Paciente salvar(Paciente paciente) {
+        return repository.save(paciente);
+    }
+
+    public List<Paciente> listarTodos() {
+        return repository.findAll();
+    }
+
+    public Optional<Paciente> buscarPorId(Long id) {
+        return repository.findById(id);
+    }
+
+    public void deletar(Long id) {
+        repository.deleteById(id);
+    }
+}
